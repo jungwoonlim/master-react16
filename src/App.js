5c578a4c0e09a5e4069148a56786b1f54a0529fc
jungwoonlim <wypo23@naver.com>
2019-06-27 17:02:49 +0900

    Error Boundaries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React, { Component, Fragment } from "react";
 import { createPortal } from "react-dom";
 
+class ErrorMaker extends Component {
+  state = {
+    frineds: ["L", "J", "W", "cloud"]
+  };
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        frineds: undefined
+      });
+    }, 2000);
+  };
+  render() {
+    const { frineds } = this.state;
+    return frineds.map(frined => ` ${frined} `);
+  }
+}
+
 class Portals extends Component {
   render() {
     return createPortal(<Message />, document.getElementById("touchme"));
@@ -15,12 +32,25 @@ class ReturnTypes extends Component {
   }
 }
 
+const ErrorFallback = () => "sry";
+
 class App extends Component {
+  state = {
+    hasError: false
+  };
+  componentDidCatch = (error, info) => {
+    console.log(`catched ${error} the info i have is ${JSON.stringify(info)}`);
+    this.setState({
+      hasError: true
+    });
+  };
   render() {
+    const { hasError } = this.state;
     return (
       <Fragment>
         <ReturnTypes />
         <Portals />
+        {hasError ? <ErrorFallback /> : <ErrorMaker />}
       </Fragment>
     );
   }
